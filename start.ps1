aa87ecb2ab534b8c438db64e7dcaab7bc8339176
Antti <kukkant@gmail.com>
2022-02-26 19:55:20 +0200

    final

---
+++ start.ps1
@@ -0,0 +1,8 @@
+ionic g page pages/tabs
+ionic g page pages/mail
+ionic g page pages/meet
+ionic g page pages/account
+ionic g page pages/details
+
+ionic g module directives/sharedDirectives --flat
+ionic g directive directives/hideHeader
